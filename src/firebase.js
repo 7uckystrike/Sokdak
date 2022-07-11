2e2e22e4aea1454bc8be404290fe4bb012af0be3
yeoung <yeoung.dev@gmail.com>
2022-07-11 23:55:02 +0900

    🕊 clone twitter react and firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdUIITcPrHMLa5ASJuPkBvuuHU8Rw6nv4",
-  authDomain: "nwitter-5ad39.firebaseapp.com",
-  projectId: "nwitter-5ad39",
-  storageBucket: "nwitter-5ad39.appspot.com",
-  messagingSenderId: "196756277423",
-  appId: "1:196756277423:web:058ca9103f9f38af11ec5a"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID, 
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
